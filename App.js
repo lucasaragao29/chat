89eaf16c53e3255450ef91ed50631e4938261136
lucasaragao29 <95982348+lucasaragao29@users.noreply.github.com>
2023-03-28 09:46:09 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,20 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, Text, View , StyleSheet, TextInput , KeyboardAvoidingView} from 'react-native'
+import React, { Component } from 'react'
+import styles from './src/css/style'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+export default class App extends Component {
+  render() {
+    return (
+     <SafeAreaView style={styles.sf}>
+      <View style={styles.view}>
+        <Text style={styles.nome}>Lucas Aragão</Text>
+      </View>
+
+      <View style={styles.chat}>
+        <Text style={styles.msg}>Mussum Ipsum, cacilds vidis litro abertis. Mauris nec dolor in eros commodo tempor.</Text>
+        </View>
+    
+      
+      <View style={styles.inputArea}>
+        <TextInput
+          style={styles.inputTxt}
+          multiline
+          keybo
+          value='Mussum Ipsum, cacilds vidis litro abertis. Mauris nec dolor in eros commodo tempor.'
+        />
+      </View>
+     </SafeAreaView>
+    )
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
